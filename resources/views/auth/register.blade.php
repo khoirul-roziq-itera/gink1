18d7244a9c76830fd4aa4f8353435fc4f6b09c13
dwikias <dwikiagussaputra84@gmail.com>
2021-07-13 10:43:24 +0700

    updte register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="{{ asset('stisla/node_modules/selectric/public/selectric.css') }}">
 
     <!-- Template CSS -->
-    <link rel="stylesheet" href="{{ asset('stisla/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('stisla/assets/css/components.css') }}">
 </head>
 
@@ -25,7 +25,7 @@
                 <div class="row">
                     <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
                         <div class="login-brand">
-                            <img src="{{ asset('stisla/assets/img/ginktech.jpg') }}" alt="logo" width="150" class="shadow-light rounded-circle">
+                            <img src="{{ asset('img/ginktech.jpg') }}" alt="logo" width="150" class="shadow-light rounded-circle">
                         </div>
 
                         <div class="card card-danger">
