2b30ee7db99d949f1bc3d6fe2aaa83952bb2a02d
khoirul-roziq-itera <khoirul.118140142@student.itera.ac.id>
2021-08-11 01:01:24 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -99,31 +99,18 @@ Route::get('detailCateg', function () {
 });
 
 // vIEW ADMIN
-<<<<<<< HEAD
-Route::get('admins', function () {
+Route::get('users', function () {
     return view('/users/index');
 });
-Route::get('addadmin', function () {
+Route::get('addusers', function () {
     return view('/users/create');
 });
-Route::get('detailadmin', function () {
+Route::get('detailusers', function () {
     return view('/users/detail');
 });
-
-=======
-Route::get('users', function () {
-     return view('/users/index');
- });
-Route::get('addusers', function () {
-     return view('/users/create');
- });
-Route::get('detailusers', function () {
-     return view('/users/detail');
- });
 Route::get('editusers', function () {
-     return view('/users/edit');
+    return view('/users/edit');
 });
->>>>>>> f222b8362d2a561d704a022d3c1ff8f52035190a
 
 Route::group(['middleware' => ['auth', 'userlevel:admin']], function () {
     Route::resource('/users', UsersController::class);
