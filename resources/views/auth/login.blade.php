43e1ca6c33150d4d4168959b4b1bea728e916191
dwikias <dwikiagussaputra84@gmail.com>
2021-09-02 13:32:58 +0700

    menambahkan background pada halaman login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,12 +9,23 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
+  <style>
+    body {
+
+    background-image : url('img/bg2.jpg');
+    background-repeat: no-repeat;
+    background-size  : cover;
+    background-color : #cccccc;
+
+    }
+  </style>
   <!-- Template CSS -->
   <link rel="stylesheet" href="{{asset('css/style.css')}}">
   <link rel="stylesheet" href="{{asset('css/components.css')}}">
 </head>
 
 <body>
+  
   <div id="app">
     <section class="section">
       <div class="container mt-5">
